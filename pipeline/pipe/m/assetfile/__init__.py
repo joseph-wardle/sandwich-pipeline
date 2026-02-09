aa5073893051270cb94081d9101097b6522a6b04
Joseph Matthew Wardle <joseward@byu.edu>
2026-02-09 14:49:05 -0700

    Add asset code information to asset files

---
+++ pipeline/pipe/m/assetfile/__init__.py
@@ -0,0 +1,29 @@
+"""Maya asset file helpers and UI."""
+
+from .assetfile_manager import (
+    FILEINFO_ASSET_DISPLAY_NAME,
+    FILEINFO_ASSET_ID,
+    FILEINFO_ASSET_NAME,
+    FILEINFO_ASSET_PATH,
+    FILEINFO_PREFIX,
+    AssetMetadata,
+    AssetOpenDialog,
+    MAssetFileManager,
+    install_asset_menu,
+    read_asset_metadata,
+    write_asset_metadata,
+)
+
+__all__ = [
+    "FILEINFO_PREFIX",
+    "FILEINFO_ASSET_ID",
+    "FILEINFO_ASSET_NAME",
+    "FILEINFO_ASSET_DISPLAY_NAME",
+    "FILEINFO_ASSET_PATH",
+    "AssetMetadata",
+    "write_asset_metadata",
+    "read_asset_metadata",
+    "AssetOpenDialog",
+    "MAssetFileManager",
+    "install_asset_menu",
+]
