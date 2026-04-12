dda948e24f4268762bb12a640bdbbcec80ad7cb4
Michael Harmon <mfh.scoobydoo@gmail.com>
2026-04-11 18:41:43 -0600

    Add Pipeline Menu

--- pipeline/pipe/b/test.py
+++ pipeline/pipe/b/test.py
@@ -11,6 +11,11 @@ class HelloWorld(bpy.types.Operator):
     bl_idname = "wm.pipeline_helloworld"
     bl_label = "Hello World!"
     bl_options = {"REGISTER", "UNDO"}
+    bl_description = "Prints to the console"
+
+    @classmethod
+    def poll(cls, context):
+        return True
 
     def execute(self, context: Context):
         print("Hello World!")
--- pipeline/software/blender/scripts/addons/pipeline/__init__.py
+++ pipeline/software/blender/scripts/addons/pipeline/__init__.py
@@ -1,5 +1,6 @@
 import logging
 
+import bpy
 from bpy.types import Operator
 from bpy.utils import register_class, unregister_class
 from pipe.b.operator import get_decorated_operators
@@ -11,16 +12,53 @@ registered_operators: set[type[Operator]] = set()
 log = logging.getLogger("pipe.b.addon")
 
 
+class PIPELINE_PT_tools(bpy.types.Panel):
+    bl_label = "Pipeline Tools"
+    bl_idname = "PIPELINE_PT_tools"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Pipeline"
+
+    def draw(self, context):
+        layout = self.layout
+        if layout is None:
+            return
+        for operator in registered_operators:
+            layout.operator(operator.bl_idname)
+
+
+class PIPELINE_MT_menu(bpy.types.Menu):
+    bl_label = "Pipeline"
+    bl_idname = "PIPELINE_MT_menu"
+
+    def draw(self, context):
+        layout = self.layout
+        if layout is None:
+            return
+        for operator in registered_operators:
+            layout.operator(operator.bl_idname)
+
+
+def draw_pipeline(self, context):
+    self.layout.menu(PIPELINE_MT_menu.bl_idname)
+
+
 def register():
     global registered_operators
     registered_operators = get_decorated_operators()
     for operator in registered_operators:
         register_class(operator)
         log.debug(f"{operator} registered as operator.")
+    bpy.utils.register_class(PIPELINE_MT_menu)
+    bpy.utils.register_class(PIPELINE_PT_tools)
+    bpy.types.TOPBAR_MT_editor_menus.append(draw_pipeline)
     log.info("Pipeline addon loaded!")
 
 
 def unregister():
+    bpy.types.TOPBAR_MT_editor_menus.remove(draw_pipeline)
+    bpy.utils.unregister_class(PIPELINE_PT_tools)
+    bpy.utils.unregister_class(PIPELINE_MT_menu)
     for operator in registered_operators:
         unregister_class(operator)
     log.info("Pipeline addon unloaded!")
