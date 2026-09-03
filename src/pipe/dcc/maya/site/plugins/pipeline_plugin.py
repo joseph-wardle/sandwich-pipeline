9fedb3010be9c7d4e106dcd2762b7419bfd0d218
Michael Harmon <mfh.scoobydoo@gmail.com>
2026-09-02 20:41:14 -0600

    add nxt_editor

---
+++ src/pipe/dcc/maya/site/plugins/pipeline_plugin.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import logging
+
+from maya import cmds
+from maya.api.OpenMaya import MFnPlugin, MObject
+from pipe.dcc.maya.command import (
+    add_named_command,
+    get_decorated_commands,
+    register_command_from_description,
+)
+from pipe.dcc.maya.util.selection import CTX_CMD_NAME, IslandFaceSelectContextCommand
+
+log = logging.getLogger("pipe.dcc.maya.pipeline_plugin")
+
+PLUGIN_DISPLAY_NAME = "Sandwich Pipeline"
+COMMAND_PREFIX = "SKD_"
+HOTKEY_SET_NAME = "Sandwich_Pipeline"
+
+CUSTOM_HOTKEYS_TO_ADD: dict[str, str] = {"CreateMotionTrail": "ctrl+alt+m"}
+
+maya_useNewAPI = True  # Tell Maya to use the Python API 2.0
+
+
+# --- Standard Maya plug-in entry points ---
+def initializePlugin(plugin: MObject) -> None:
+    plugin_fn = MFnPlugin(plugin)
+    plugin_fn.registerContextCommand(
+        CTX_CMD_NAME, IslandFaceSelectContextCommand.creator
+    )
+    if not cmds.hotkeySet(HOTKEY_SET_NAME, query=True, exists=True):
+        cmds.hotkeySet(HOTKEY_SET_NAME, current=True)
+    else:
+        cmds.hotkeySet(HOTKEY_SET_NAME, edit=True, current=True)
+
+    for command in get_decorated_commands():
+        register_command_from_description(
+            command, prefix=COMMAND_PREFIX, base_category="Pipeline", default=True
+        )
+
+    for command, hotkey in CUSTOM_HOTKEYS_TO_ADD.items():
+        add_named_command(command, command, hotkey)
+
+    log.info(f"{PLUGIN_DISPLAY_NAME} initialized")
+
+
+def uninitializePlugin(plugin: MObject) -> None:
+    plugin_fn = MFnPlugin(plugin)
+    plugin_fn.deregisterContextCommand(CTX_CMD_NAME)
+    log.info(f"{PLUGIN_DISPLAY_NAME} un-initialized")
